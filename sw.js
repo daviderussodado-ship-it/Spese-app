ef8e664297a1080a32216a7e7b5848a194de64f9
daviderussodado-ship-it <davide.russo.dado@gmail.com>
2026-09-20 09:34:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spese-pwa-v19-4-1-updatefix';
+const CACHE='spese-pwa-v20-0-beta-redesign';
 const CORE=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
